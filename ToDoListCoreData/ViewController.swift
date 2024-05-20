5a20c89c56f63b077168e3627278e8f68e4f00a0
deshollow <555demin@gmail.com>
2024-05-20 13:17:08 +0300

    Initial Commit

---
+++ ToDoListCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListCoreData
+//
+//  Created by deshollow on 20.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
